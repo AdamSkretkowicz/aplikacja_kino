8b5ba60c7fc2a241d94efba51bc5f20f736dd530
Adam Skretkowicz <adam.skretkowicz@gmail.com>
2016-01-06 23:07:00 +0100

    [6] java.class uzupelnienie extends + Dodanie opisu dla onclick

--- app/src/main/java/com/skretkowicz/kino/Edycja.java
+++ app/src/main/java/com/skretkowicz/kino/Edycja.java
@@ -1,9 +1,15 @@
 package com.skretkowicz.kino;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Adam on 2016-01-06.
  */
 public class Edycja extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edycja);
+    }
 }
