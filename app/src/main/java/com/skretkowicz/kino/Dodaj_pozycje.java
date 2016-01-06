e4decaca1f9e6a4ac6d6fa9b534892eedd5120eb
Adam Skretkowicz <adam.skretkowicz@gmail.com>
2016-01-06 22:08:34 +0100

    [1] Podstawy dla projektu KINO. Zawartość czysta java.class + activity

---
+++ app/src/main/java/com/skretkowicz/kino/Dodaj_pozycje.java
@@ -0,0 +1,7 @@
+package com.skretkowicz.kino;
+
+/**
+ * Created by Adam on 2016-01-06.
+ */
+public class Dodaj_pozycje {
+}
---
+++ app/src/main/java/com/skretkowicz/kino/Filmy_do_obejrzenia.java
@@ -0,0 +1,13 @@
+package com.skretkowicz.kino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Filmy_do_obejrzenia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filmy_do_obejrzenia);
+    }
+}
